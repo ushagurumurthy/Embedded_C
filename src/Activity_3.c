4d74d94308628b55f7dcfc0f592cb99e8f452029
Usha B G <67991189+ushagurumurthy@users.noreply.github.com>
2021-04-28 14:02:16 +0530

    Create Activity_3.c

---
+++ src/Activity_3.c
@@ -0,0 +1,8 @@
+#include "Activity_1.h"
+#include "Activity_3.h"
+
+void InitializePWM(void){
+    TCCR1A=(1<<COM1A1)|(1<<WGM11)|(1<<WGM10);
+    TCCR1B=(1<<CS11)|(1<<WGM12)|(1<<CS10);
+    DDRB|=(1<<PB1);
+}
