d4e26c86efeb061835668fb64f2c3af11f7aee10
Usha B G <67991189+ushagurumurthy@users.noreply.github.com>
2021-04-28 13:59:08 +0530

    Create Activity_3.h

---
+++ inc/Activity_3.h
@@ -0,0 +1,21 @@
+#ifndef _ACTIVITY_3_H_
+#define _ACTIVITY_3_H_
+
+/**
+ * @file Activity_3.h
+ * @author UshaBG
+ * @brief Activity_3
+ * @version 0.1
+ * @date 2021
+ * 
+ * @copyright Copyright (c) 2021
+ * 
+ */
+
+
+/**
+ * @brief Function to initialize PWM    
+ * 
+ */
+void InitializePWM(void);
+#endif  /* _ACTIVITY3_H_ */
