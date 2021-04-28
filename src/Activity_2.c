5ec5400cebf45755d1674f8ecebb36b821fa4761
Usha B G <67991189+ushagurumurthy@users.noreply.github.com>
2021-04-28 14:01:06 +0530

    Create Activity_2.c

---
+++ src/Activity_2.c
@@ -0,0 +1,26 @@
+#include <avr/io.h>
+#include <util/delay.h>
+#include "Activity_2.h"
+
+#define CONVERSION_INCOMPLETE  ADCSRA & (1<<ADSC)
+
+void InitializeADC()
+{
+    // Select Vref=AVcc
+    ADMUX |= (1<<REFS0);
+    //set prescaller to 128 and enable ADC
+    ADCSRA |= (1<<ADPS2)|(1<<ADPS1)|(1<<ADPS0)|(1<<ADEN);
+}
+uint16_t ReadADC(uint8_t channel)
+{
+    //select ADC channel with safety mask
+    ADMUX &= 0xF8;
+    //single conversion mode
+    channel &= 0x07;
+    ADMUX &= channel;
+    ADCSRA |= (1<<ADSC);
+    // wait until ADC conversion is complete
+    while(CONVERSION_INCOMPLETE);
+    ADCSRA|=(1<<ADIF);
+    return ADC;
+}
