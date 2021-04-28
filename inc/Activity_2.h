91d636c8551d86da562214659a679b7d2b361bf8
Usha B G <67991189+ushagurumurthy@users.noreply.github.com>
2021-04-28 13:57:54 +0530

    Create Activity_2.h

---
+++ inc/Activity_2.h
@@ -0,0 +1,28 @@
+#ifndef _ACTIVITY_2_H_
+#define _ACTIVITY_2_H_
+
+/**
+ * @file Activity_2.h
+ * @author UshaBG
+ * @brief Activity_2
+ * @version 0.1
+ * @date 2021
+ * 
+ * @copyright Copyright (c) 2021
+ * 
+ */
+
+
+/**
+ * @brief Function to initialize ADC
+ * 
+ */
+void InitializeADC();
+
+/**
+ * @brief Function to read values from a channel 
+ * @param[in] channel channel number
+ */
+uint16_t ReadADC(uint8_t channel);
+
+#endif /* _ACTIVITY_2_H_ */
