7c49d9927548db7e4754ba59072665c5573fb589
Usha B G <67991189+ushagurumurthy@users.noreply.github.com>
2021-04-29 23:23:33 +0530

    Update Activity_4.h

--- inc/Activity_4.h
+++ inc/Activity_4.h
@@ -1,1 +1,26 @@
+#ifndef _ACTIVITY4_H_
+#define _ACTIVITY4_H_
 
+/**
+ * @file Activity_4.h
+ * @author Usha B G ()
+ * @brief Activity4
+ * @version 0.1
+ * @date 2021-04-29
+ * 
+ * @copyright Copyright (c) 2021
+ * 
+ */
+/**
+ * @brief Function to initialize UART 
+ * 
+ */
+void USARTInit(void);
+
+/**
+ * @brief Function to write string  
+ * @param[in] a single character
+ */
+void USARTWriteString(char);
+
+#endif  /* _ACTIVITY4_H_ */
