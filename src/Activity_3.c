8769ffd360686166b48a9ff3917650f0a6338b1d
Usha B G <67991189+ushagurumurthy@users.noreply.github.com>
2021-04-29 09:36:24 +0530

    Update Activity_3.c

--- src/Activity_3.c
+++ src/Activity_3.c
@@ -6,3 +6,23 @@ void InitializePWM(void){
     TCCR1B=(1<<CS11)|(1<<WGM12)|(1<<CS10);
     DDRB|=(1<<PB1);
 }
+
+void GeneratePWM(uint16_t Temp){
+    InitializePWM();
+    if(Temp>=0 && Temp<=200){
+        OCR1A = 204.8;
+        DelayMilliSecond(200);
+    }
+    else if(Temp>=201 && Temp<=500){
+        OCR1A = 409.6;
+        DelayMilliSecond(200);
+    }
+    else if(Temp>=501 && Temp<=700){
+        OCR1A = 716.8;
+        DelayMilliSecond(200);
+    }
+    else{
+        OCR1A = 972.8;
+        DelayMilliSecond(200);
+    }
+}
