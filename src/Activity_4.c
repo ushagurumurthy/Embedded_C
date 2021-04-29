d47b8ccb16dd555891daa35c24d607e2980ed43f
Usha B G <67991189+ushagurumurthy@users.noreply.github.com>
2021-04-29 23:33:36 +0530

    Update Activity_4.c

--- src/Activity_4.c
+++ src/Activity_4.c
@@ -1,7 +1,7 @@
 #define BAUD 9600 // define baud 
 #define BAUDRATE ((F_CPU)/(BAUD*16UL)-1) // set baud rate value for UBRR
-#include "activity4.h"
-#include "activity1.h"
+#include "Activity_4.h"
+#include "Activity_1.h"
 #include <avr/interrupt.h>
 #define DATA_IS_TRANSMITTING !(UCSR0A & (1<<UDRE0))
 #define DATA_IS_RECEIVING !(UCSR0A & (1<<RXC0))
@@ -14,24 +14,6 @@ void USARTInit(void){
     UCSR0C=(1<<UCSZ01)|(1<<UCSZ00); //Enable The receiver and transmitter 
     UCSR0B=(1<<RXEN0)|(1<<TXEN0)|(1<<RXCIE0)|(1<<TXCIE0); 
 }
-/*
-char USARTReadChar(){ 
-    USARTInit(); // Initialize Peripherals for UART 
-    while(DATA_IS_RECEIVING) //Wait untill a data is available 
-    {
-    //Do nothing
-    }
-    return DATA; 
-}
-
-void USARTWriteChar(char letter){
-    while(DATA_IS_TRANSMITTING) //Wait untill the transmitter is ready 
-    {
-    //Do nothing
-    }
-    DATA = letter; //write the data to USART buffer 
-}
-*/
 
 void USARTWriteString(char letter){
     if(letter=='a'){
